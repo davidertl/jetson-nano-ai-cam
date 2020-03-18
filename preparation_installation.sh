8bc98fe7834d9e39a7d3a0aa9e50c803955f64ae
Jetson User <jetson-rw@mail2you.net>
2020-03-18 12:09:33 -0700

    added requirement.sh

---
+++ preparation_installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+~/skip_sudo.sh
+sudo apt-get update
+sudo apt-get install v4l-utils
+sudo apt-get install gawk curl
+
+sudo apt-get install python3-pip libhdf5-serial-dev hdf5-tools
+
+
+
+sudo systemctl set-default multi-user.target
+
+sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+sudo apt-get install -y curl
+
+
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+
+sudo apt-get install -y build-essential libatlas-base-dev
+sudo apt-get install python3-scipy
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+
+sudo apt-get install python3-pip
+
+#sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
+
+sudo pip3 install -U pip testresources setuptools
+
+#sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+
+
